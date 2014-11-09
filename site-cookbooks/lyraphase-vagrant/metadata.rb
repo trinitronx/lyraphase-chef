43ff2d0493fbfbac04d0533492dbcf29b1e5e1ea
James Cuzella <james.cuzella@lyraphase.com>
2014-11-08 17:40:44 -0700

    Adding first metadata for lyraphase-vagrant cookbook

---
+++ site-cookbooks/lyraphase-vagrant/metadata.rb
@@ -0,0 +1,8 @@
+name             'lyraphase-vagrant'
+maintainer       'James Cuzella'
+maintainer_email 'james.cuzella@lyraphase.com'
+license          'Apache 2.0'
+description      'Installs/Configures vagrant on OSX and Ubuntu'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md')) 
+version          '0.1.0'
+
