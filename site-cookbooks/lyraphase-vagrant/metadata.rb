3bfb094bc199130ff7c7f3aefbe130f199aea78e
James Cuzella <james.cuzella@lyraphase.com>
2014-11-08 17:43:35 -0700

    Add lyraphase-vagrant Wrapper Cookbook dependencies
      - vagrant ~> 0.2 - for debian, ubuntu, redhat
      - sprout-homebrew ~> 0.1 - for osx

--- site-cookbooks/lyraphase-vagrant/metadata.rb
+++ site-cookbooks/lyraphase-vagrant/metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures vagrant on OSX and Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md')) 
 version          '0.1.0'
 
+depends          'vagrant', '~> 0.2'
+depends          'sprout-homebrew', '~> 0.1'
